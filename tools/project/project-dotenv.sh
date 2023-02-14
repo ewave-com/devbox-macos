15a80efbb1831781279b9e052f6eed4f4da99500
George Bukhtatyi <george.bukhtaty@ewave.com>
2023-02-14 15:09:31 +0300

    Switch to madebyewave unison image

--- tools/project/project-dotenv.sh
+++ tools/project/project-dotenv.sh
@@ -356,9 +356,9 @@ function add_internal_generated_prams() {
   # define used docker-sync docker image for unison
   if [[ -z "$(dotenv_get_param_value 'DOCKER_SYNC_UNISON_IMAGE')" ]]; then
     if [[ ${arch_type} == 'arm64' ]]; then
-      dotenv_set_param_value 'DOCKER_SYNC_UNISON_IMAGE' "eugenmayer/unison:2.51.3-4.12.0-ARM64"
+      dotenv_set_param_value 'DOCKER_SYNC_UNISON_IMAGE' "madebyewave/unison:2.51.3-4.12.0"
     else
-      dotenv_set_param_value 'DOCKER_SYNC_UNISON_IMAGE' "eugenmayer/unison:2.51.3-4.12.0-AMD64"
+      dotenv_set_param_value 'DOCKER_SYNC_UNISON_IMAGE' "madebyewave/unison:2.51.3-4.12.0"
     fi
   fi
 }
